778f6ef5ff42b31a4a0ad72ef6364cc3f97fb497
RoseMagura <magurarm@guilford.edu>
2020-10-04 12:05:58 -0400

    Edit: Simplify file structure

---
+++ src/main/java/com/udacity/critter/schedule/Schedule.java
@@ -0,0 +1,69 @@
+package com.udacity.critter.schedule;
+
+
+import com.udacity.critter.pet.Pet;
+import com.udacity.critter.user.Employee;
+import com.udacity.critter.user.EmployeeSkill;
+
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+public class Schedule {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "employee_id")
+    private Set<Employee> employees;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "pet_id")
+    private Set<Pet> pets;
+
+    private LocalDate date;
+    @ElementCollection
+    private Set<EmployeeSkill> activities;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Set<EmployeeSkill> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(Set<EmployeeSkill> activities) {
+        this.activities = activities;
+    }
+}
---
+++ src/main/java/com/udacity/critter/user/UserRepository.java
@@ -0,0 +1,49 @@
+package com.udacity.critter.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+//@Transactional
+public interface UserRepository extends JpaRepository<User, Long> {
+    //    @PersistenceContext
+//    EntityManager entityManager;
+//
+//    public void persist(Customer customer) {
+//        entityManager.persist(customer);
+//    }
+//
+//    public Customer findCustomer(Long id) {
+//        return entityManager.find(Customer.class, id);
+//    }
+//
+//    public Customer merge(Customer customer){
+//        return entityManager.merge(customer);
+//    }
+//
+//    public void deleteCustomer(Long id) {
+//        Customer customer = entityManager.find(Customer.class, id);
+//        entityManager.remove(customer);
+//    }
+//    public void persist(Employee employee) {
+//        entityManager.persist(employee);
+//    }
+//
+//    public Employee find(Long id) {
+//        return entityManager.find(Employee.class, id);
+//    }
+//
+//    public Employee merge(Employee employee){
+//        return entityManager.merge(employee);
+//    }
+//
+//    public void delete(Long id) {
+//        Employee employee = entityManager.find(Employee.class, id);
+//        entityManager.remove(employee);
+//    }
+}
